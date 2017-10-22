683870863162f527ad79d7872fc6a0a7518bbaa8
airbusramo <omarrefaat15@live.com>
2017-10-22 17:14:51 +0200

    Added first bit of math and got user inputs working.

--- Vote-Comparer-BTS/Program.cs
+++ Vote-Comparer-BTS/Program.cs
@@ -10,12 +10,21 @@ namespace Vote_Comparer_BTS
         static void Main(string[] args)
         {
 
-         Console.Write("BTS's old amount of votes:");
+            Console.Write("BTS's old amount of votes:");
+            string BTS_Old_Input = Console.ReadLine();
 
-         string BTS_Old = Console.ReadLine();
+            Console.Write("BTS's new amount of votes:");
+            string BTS_New_Input = Console.ReadLine();
 
-         Console.WriteLine(BTS_Old);
+            int BTS_Old_Int;
+            int BTS_New_Int;
 
+            Int32.TryParse(BTS_Old_Input, out BTS_Old_Int);
+            Int32.TryParse(BTS_New_Input, out BTS_New_Int);
+
+            int BTS_Change = BTS_New_Int - BTS_Old_Int;
+
+            Console.WriteLine(BTS_Change);
         }
     }
 }
