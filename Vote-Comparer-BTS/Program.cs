3fa532e130175172e2667649064c253ba1cfe848
airbusramo <omarrefaat15@live.com>
2017-10-22 18:20:33 +0200

    Added competitor.

--- Vote-Comparer-BTS/Program.cs
+++ Vote-Comparer-BTS/Program.cs
@@ -16,15 +16,28 @@ namespace Vote_Comparer_BTS
             Console.Write("BTS's new amount of votes:");
             string BTS_New_Input = Console.ReadLine();
 
+            Console.Write("Competitor's Old amount of votes:");
+            string Competitor_Old_Input = Console.ReadLine();
+
+            Console.Write("Competitor's new amount of votes:");
+            string Competitor_New_Input = Console.ReadLine();
+
             int BTS_Old_Int;
             int BTS_New_Int;
+            int Competitor_Old_Int;
+            int Competitor_New_Int;
 
+            //Converting from string input to int.
             Int32.TryParse(BTS_Old_Input, out BTS_Old_Int);
             Int32.TryParse(BTS_New_Input, out BTS_New_Int);
+            Int32.TryParse(Competitor_Old_Input, out Competitor_Old_Int);
+            Int32.TryParse(Competitor_New_Input, out Competitor_New_Int);
 
             int BTS_Change = BTS_New_Int - BTS_Old_Int;
+            int Competitor_Change = Competitor_New_Int - Competitor_Old_Int;
 
-            Console.WriteLine(BTS_Change);
+            Console.WriteLine("BTS change is " + BTS_Change);
+            Console.WriteLine("Competitor change is " + Competitor_Change);
         }
     }
 }
